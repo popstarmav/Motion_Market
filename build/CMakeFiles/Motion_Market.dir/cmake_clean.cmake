5f49272255be9c72967594549409aaea52f38f27
popstarmav <cj.johnson2017@gmail.com>
2024-12-03 10:36:17 -0800

    fix external problems and need to update main.cpp

---
+++ build/CMakeFiles/Motion_Market.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Motion_Market.dir/external/glad/src/glad.c.o"
+  "CMakeFiles/Motion_Market.dir/external/glad/src/glad.c.o.d"
+  "CMakeFiles/Motion_Market.dir/src/artists.cpp.o"
+  "CMakeFiles/Motion_Market.dir/src/artists.cpp.o.d"
+  "CMakeFiles/Motion_Market.dir/src/main.cpp.o"
+  "CMakeFiles/Motion_Market.dir/src/main.cpp.o.d"
+  "Motion_Market"
+  "Motion_Market.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Motion_Market.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Motion_Market.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Motion_Market.
+# This may be replaced when dependencies are built.
