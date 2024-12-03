dc12fc56e424dff6ee2cc47eb028e0840bd819d9
popstarmav <cj.johnson2017@gmail.com>
2024-12-02 22:48:46 -0800

    flutter not work for c++ using imgui

--- flutter_application_1/linux/blockchain.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLOCKCHAIN_H
-#define BLOCKCHAIN_H
-
-const char* get_blockchain_message();
-
-#endif
--- flutter_application_1/linux/graph.h
+++
@@ -1,6 +0,0 @@
-#ifndef GRAPH_H
-#define GRAPH_H
-
-const char* get_graph_message();
-
-#endif
